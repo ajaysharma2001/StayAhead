f2f93e61188d8bc63acc75f72c756ecaa95b6280
Ajay Sharma <ajayks@msn.com>
2019-12-17 17:11:58 -0500

    Initial Commit

---
+++ StayAhead/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StayAhead
+//
+//  Created by Ajay Sharma on 2019-12-17.
+//  Copyright © 2019 MrGoose. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
