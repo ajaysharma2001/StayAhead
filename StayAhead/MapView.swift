37f8f7ed3a61cbce14b9403fdee61bd0b180baac
Ajay Sharma <ajayks@msn.com>
2019-12-18 13:28:31 -0500

    Map can get location of touch

--- StayAhead/MapView.swift
+++ StayAhead/MapView.swift
@@ -12,31 +12,53 @@ import UIKit
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    func makeCoordinator() -> MapView.Coordinator {
+        Coordinator()
+    }
+    
     let marker : GMSMarker = GMSMarker()
     @State var setStart: Bool
     @State var setEnd: Bool
+    
+    let startTap = 250
+    let endTap = 250
+    
 
     /// Creates a `UIView` instance to be presented.
     func makeUIView(context: Self.Context) -> GMSMapView {
+        
+        
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
+
+        
         let camera = GMSCameraPosition.camera(withLatitude: 43.477768, longitude: -80.555584, zoom: 14.5)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
 
+        mapView.delegate = context.coordinator
         return mapView
     }
+    
 
     /// Updates the presented `UIView` (and coordinator) to the latest
     /// configuration.
     func updateUIView(_ mapView: GMSMapView, context: Self.Context) {
         // Creates a marker in the center of the map.
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
     }
+    
+    class Coordinator: NSObject, GMSMapViewDelegate {
+        func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+            print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+        }
+        
+    }
 
 }
+
 
 
 struct MapView_Previews: PreviewProvider {
